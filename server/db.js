40bddbeaef46a26541e01c2bae77fd7ea8ae5373
leolperes <leo.lperes@gmail.com>
2024-06-11 12:20:04 -0300

    change db password

--- server/db.js
+++ server/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password:"mypassword",
+    password:"123",
     database:"my_first_project"
 })
